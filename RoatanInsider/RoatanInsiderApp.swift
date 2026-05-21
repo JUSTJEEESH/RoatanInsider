a2f40662abc092bbbc70056c342ebdcb406d08bc
JUSTJEEESH <joshgreendesignstudio@gmail.com>
2026-05-20 18:15:14 -0600

    sentry: install SDK + dSYM upload via Sentry wizard

--- RoatanInsider/RoatanInsiderApp.swift
+++ RoatanInsider/RoatanInsiderApp.swift
@@ -1,4 +1,6 @@
 import SwiftUI
+import Sentry
+
 import SwiftData
 import UIKit
 import os
@@ -19,6 +21,33 @@ struct RoatanInsiderApp: App {
     private let favoritesStore: FavoritesStore
 
     init() {
+        SentrySDK.start { options in
+            options.dsn = "https://2fae8a0d2affb47b0f9dfa47cf522505@o4511424942374912.ingest.us.sentry.io/4511424946503680"
+
+            // Adds IP for users.
+            // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
+            options.sendDefaultPii = true
+
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+
+            // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
+            options.configureProfiling = {
+                $0.sessionSampleRate = 1.0 // We recommend adjusting this value in production.
+                $0.lifecycle = .trace
+            }
+
+            // Uncomment the following lines to add more data to your events
+            // options.attachScreenshot = true // This adds a screenshot to the error events
+            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+            
+            // Enable experimental logging features
+            options.experimental.enableLogs = true
+        }
+        // Remove the next line after confirming that your Sentry integration is working.
+        SentrySDK.capture(message: "This app uses Sentry! :)")
+
         // Schema V2 is CloudKit-compatible. The migration plan carries forward
         // any V1 favorites from existing installs without data loss.
         let schema = Schema(versionedSchema: FavoriteSchemaV2.self)
